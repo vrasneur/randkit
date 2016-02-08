a97ecd3c6e7b87884c208a2dfaccb6aa666135cd
Vincent Rasneur <vrasneur@free.fr>
2016-02-08 19:56:27 +0100

    add a tool to find keys generated by the backdoored PRNG

---
+++ tools/unrandom.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+
+import os
+import struct
+import subprocess
+
+class Xor128(object):
+    MASK = 2**32 - 1
+
+    def __init__(self, x=123456789, y=362436069, z=521288629, w=88675123):
+        self.x = x
+        self.y = y
+        self.z = z
+        self.w = w
+
+    @classmethod
+    def init_from_urandom(cls):
+        rnd = os.urandom(16)
+        x = struct.unpack('<L', rnd[0:4])[0]
+        y = struct.unpack('<L', rnd[4:8])[0]
+        z = struct.unpack('<L', rnd[8:12])[0]
+        w = struct.unpack('<L', rnd[12:16])[0]
+        
+        return cls(x=x, y=y, z=z, w=w)
+        
+    @classmethod
+    def reverse_xor_lshift(cls, y, shift):
+        x = y
+        idx = shift
+
+        while idx < 32:
+            x ^= x << idx
+            idx <<= 1
+
+        return x & cls.MASK
+
+    @classmethod
+    def reverse_xor_rshift(cls, y, shift):
+        x = y
+        idx = shift
+
+        while idx < 32:
+            x ^= x >> idx
+            idx <<= 1
+
+        return x & cls.MASK
+
+    def forward(self):
+        t = self.x ^ self.x << 11
+        t &= self.MASK
+        self.x = self.y
+        self.y = self.z
+        self.z = self.w
+
+        self.w = (self.w ^ (self.w >> 19)) ^ (t ^ (t >> 8))
+        self.w &= self.MASK
+
+        return self.w
+
+    def backward(self):
+        tmp = self.w ^ self.z ^ (self.z >> 19)
+        t = self.reverse_xor_rshift(tmp, 8)
+
+        self.w = self.z
+        self.z = self.y
+        self.y = self.x
+        self.x = self.reverse_xor_lshift(t, 11)
+
+        return self.x
+
+    def print_state(self):
+        print 'x=%d\ny=%d\nz=%d\nw=%d\n' % (self.x, self.y, self.z, self.w)
+
+    def output_state(self):
+        return struct.pack('<LLLL', self.x, self.y, self.z, self.w)
+
+    def gpg_decrypt(self, infile, outfile):
+        ret = 2
+        idx = 1
+
+        while ret != 0:
+            print 'idx: %d' % idx
+            p = subprocess.Popen('gpg -q --decrypt --passphrase-fd 0 --output %s %s' % (outfile, infile),
+                                 shell=True, stdin=subprocess.PIPE)
+            p.communicate(input=self.output_state())
+            ret = p.returncode
+            self.backward()
+            idx += 1
+        
+if __name__ == '__main__':
+    p = Xor128.init_from_urandom()
+    p.gpg_decrypt('encrypted.enc', 'plain.txt')
