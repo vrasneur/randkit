9139595284ffbee0b4571d92f686c7d07f17c51e
Vincent Rasneur <vrasneur@free.fr>
2016-02-15 19:13:41 +0100

    improve wording

--- examples/example.sh
+++ examples/example.sh
@@ -10,7 +10,7 @@ rm -f mykey encrypted.enc decrypted.txt
 echo "[*] generating 5KB of random numbers"
 dd if=/dev/urandom of=/dev/null bs=5K count=1
 
-echo "[*] generating the GPG key"
+echo "[*] generating the GPG symmetric key"
 head -c 16 </dev/urandom >mykey
 
 echo "[*] encrypting data"
@@ -24,7 +24,7 @@ dd if=/dev/urandom of=/dev/null bs=5K count=1
 
 echo "[*] decrypt the data by reversing the PRNG to retrieve the key"
 echo "[*] this should take approx. 1280 iterations"
-./unrandom.py encrypted.enc decrypted.txt
+python unrandom.py encrypted.enc decrypted.txt
 
 echo "[*] comparing the original and decrypted files:"
 diff original.txt decrypted.txt
